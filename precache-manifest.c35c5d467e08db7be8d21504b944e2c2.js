5c35919fd9efaec102cdd89fe1d13a70ec082b50
thiago <thiagoaas1990@gmail.com>
2020-05-25 19:12:59 -0300

    Updates

---
+++ precache-manifest.c35c5d467e08db7be8d21504b944e2c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0e1137fc2e6d71f8c86bba32668b33",
+    "url": "/regimento_senado/index.html"
+  },
+  {
+    "revision": "2428d4c53961ebf0edb9",
+    "url": "/regimento_senado/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "5e74d51321108005e559",
+    "url": "/regimento_senado/static/js/2.6b29211c.chunk.js"
+  },
+  {
+    "revision": "6c413564c5bd7719e2d1e3adc08f10da",
+    "url": "/regimento_senado/static/js/2.6b29211c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2428d4c53961ebf0edb9",
+    "url": "/regimento_senado/static/js/main.bac34f70.chunk.js"
+  },
+  {
+    "revision": "fd47acd73758f9d43e16",
+    "url": "/regimento_senado/static/js/runtime-main.cf42337c.js"
+  }
+]);
